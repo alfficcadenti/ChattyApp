ba3c6b72669754b26f32e43ca798b61d5ecb43d6
alfficcadenti <alfredoficcadenti@gmail.com>
2019-01-29 21:56:45 +0000

    add feature to post images jpg, gif, png

--- src/Message.jsx
+++ src/Message.jsx
@@ -5,12 +5,24 @@ class Message extends Component {
   render () {
     var styles = {color:this.props.color,}
 
+  let extension = this.props.content.substr(-3,3)
+      if ( extension === "jpg" || extension === "png" || extension === "gif") {
+        var message = <img className='image-post' src={this.props.content} />
+        }
+      else {
+        var message = <span className='message-content'>{this.props.content}</span>
+      }
+
 
     if (this.props.type === "incomingMessage" || this.props.type === "postMessage") {
+
+
+
+
       return (
         <div className='message'>
           <span className='message-username' style={styles}>{this.props.username}</span>
-          <span className='message-content'>{this.props.content}</span>
+          {message}
         </div>
         );
     }
